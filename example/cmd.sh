2a720f6ed76fd8029391b7da1342683cc09ee511
JinzhuangDou <douj@gis.a-star.edu.sg>
2017-05-16 11:29:59 +0800

    Update cmd.sh

--- example/cmd.sh
+++ example/cmd.sh
@@ -1,21 +1,20 @@
 
 
 # Run seekin for homogenous samples
-../SEEKIN_DEV1.0/bin/seekin kinship -i ./Study.10K.vcf.gz -r 0.3 -m 0.05 -d DS -p hom -l  2000 -t 3 -w 1 -o Study.hom
+../bin/seekin kinship -i ./Study.10K.vcf.gz -r 0.3 -m 0.05 -d DS -p hom -l  2000 -t 3 -w 1 -o Study.hom
 # Compare the array-based and sequence-based estimates 
 Rscript plot.r Study.hom.kin  Study.array.kin  Study.hom
 
 
 
 # Run seekin for samples with population structure and admixture
+../bin/seekin modelAF -i  SGVP.12K.vcf.gz -c SGVP.RefPC.coord -k 2  -o SGVP.beta  
 
-../SEEKIN_DEV1.0/bin/seekin modelAF -i  SGVP.12K.vcf.gz -c SGVP.RefPC.coord -k 2  -o SGVP.beta  
-
-../SEEKIN_DEV1.0/bin/seekin getAF  -i Study.onSGVP.PC.coord -b  SGVP.beta  -k 2  -o Study.10K.indvAF.vcf
+../bin/seekin getAF  -i Study.onSGVP.PC.coord -b  SGVP.beta  -k 2  -o Study.10K.indvAF.vcf
 
 tabix -p vcf ./Study.10K.indvAF.vcf.gz
 
-../SEEKIN_DEV1.0/bin/seekin kinship -i ./Study.10K.vcf.gz  -f  ./Study.10K.indvAF.vcf.gz  -r 0.3  -m 0.05   -d DS  -p het  -l 2000  -t 3 -w 1  -o Study.het
+../bin/seekin kinship -i ./Study.10K.vcf.gz  -f  ./Study.10K.indvAF.vcf.gz  -r 0.3  -m 0.05   -d DS  -p het  -l 2000  -t 3 -w 1  -o Study.het
 
 # Compare the array-based and sequence-based estimates
 
